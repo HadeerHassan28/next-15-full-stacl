517160acc27437b285047df9e5a637920b9153f6
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2025-03-12 23:21:30 +0200

    feat(middleware):apply it using kinde

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,5 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function page() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-
-  if (!user) {
-    return redirect("/api/auth/register");
-  }
   return <div>page</div>;
 }
