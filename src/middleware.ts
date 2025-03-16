4c07c29c996ce7312fe647127c53c874b3b15e71
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2025-03-16 17:48:52 +0200

    feat(middleware):change the location of middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
-export default withAuth(async function middleware(req: any) {}, {
+export default withAuth(async function middleware() {}, {
   publicPaths: ["/"],
 });
 
