386cdb6798c2e68407e605297964d696974700c9
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2025-03-17 14:53:32 +0200

    feat(dashboard):add ui in case there is no posts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import BlogPostCard from "@/components/general/BlogPostCard";
 import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { getUserPosts } from "@/data/getPosts";
 import Link from "next/link";
 import React from "react";
 
 export default async function page() {
   const data = await getUserPosts();
+  console.log(data);
+
   return (
     <div>
       <div className="flex items-center justify-between mb-4">
@@ -17,11 +20,23 @@ export default async function page() {
       </div>
 
       {/* Display user posts */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
-        {data.map((post) => (
-          <BlogPostCard data={post} key={post.id} />
-        ))}
-      </div>
+      {data.length != 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+          {data.map((post) => (
+            <BlogPostCard data={post} key={post.id} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center ">
+          <Card className="w-1/2">
+            <CardContent>
+              <p className="text-center font-medium text-gray-700">
+                There is no posts
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+      )}
     </div>
   );
 }
